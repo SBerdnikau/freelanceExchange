604c21cc9e521b41175a12ebe8f7a04229438773
Sergey Berdnikov <30774859+SBerdnikau@users.noreply.github.com>
2019-12-18 17:45:10 +0300

    day 2

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,49 @@
 document.addEventListener('DOMContentLoaded', () => {
     'use strict';
 
-    const customer = document.getElementById('customer');
-    const freelancer = document.getElementById('freelancer');
-    const blockCustomer = document.getElementById('block-customer');
-    const blockFreelancer = document.getElementById('block-freelancer');
-    const blockChoice = document.getElementById('block-choice');
-    const btnExit = document.getElementById('btn-exit');
-    const formCustomer = document.getElementById('form-customer');
+    //Злементы
+    const customer = document.getElementById('customer'),
+    freelancer = document.getElementById('freelancer'),
+    blockCustomer = document.getElementById('block-customer'),
+    blockFreelancer = document.getElementById('block-freelancer'),
+    blockChoice = document.getElementById('block-choice'),
+    btnExit = document.getElementById('btn-exit'),
+    formCustomer = document.getElementById('form-customer'),
+    ordersTabel = document.getElementById('orders');
 
     const orders = [];
+
+    //отрисовка во фриланс таблице
+    const renderOrders = () => {
+
+        ordersTabel.textContent = '';
+
+        orders.forEach( (order, i) => {
+
+            ordersTabel.innerHTML += `
+            <tr class="order" data-number-order="${i}">
+                <td>${i + 1}</td>
+                <td>${order.title}</td>
+                <td class="${order.currency}"></td>
+                <td>${order.deadline}</td>
+            </tr>`;
+
+        } );
+        
+    }
+
+    //обработчик фриланс - заказы
+    ordersTabel.addEventListener('click', event => {
+        const target = event.target;
+        const targetOrder = target.closest('.order');
+
+        if(targetOrder){
+            //открываем модальное окно
+            openModal();
+        }
+
+        console.log(orders[targetOrder.dataset.numberOrder]);
+    });
 
 
     customer.addEventListener('click', () => {
@@ -20,6 +54,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     freelancer.addEventListener('click', () => {
         blockFreelancer.style.display = 'block';
+        renderOrders();
         blockChoice.style.display = 'none';
         btnExit.style.display = 'block';
     })
@@ -37,23 +72,54 @@ document.addEventListener('DOMContentLoaded', () => {
         event.preventDefault();
         const objElem = {};
         
-        //получам из формы все элементы + значения
-        for(const elem of formCustomer.elements){
-            if(elem.tagName === "INPUT" && elem.type !== "radio" || 
-            (elem.type === "radio" && elem.checked) || 
-            elem.tagName === "TEXTAREA"  ) {
-                objElem[elem.name] = elem.value;
+        //Способ 1 получам из формы все элементы + значения
+        const elements  = [...formCustomer.elements]
+            .filter( (elem) => ( elem.tagName === "INPUT" && elem.type !== "radio" || 
+            (elem.type === "radio" && elem.checked) || elem.tagName === "TEXTAREA") );
 
-                //очищаем если не radio
-                if(elem.type !== 'radio'){
-                    elem.value = '';
-                }
+            elements.forEach( (elem) => {
+                    objElem[elem.name] = elem.value;
 
-            }
-        }
+            });
+    
+        //Способ 2 получам из формы все элементы + значения
+        // [...formCustomer.elements].forEach( (elem) => {
+        //     if(elem.tagName === "INPUT" && elem.type !== "radio" || 
+        //     (elem.type === "radio" && elem.checked) || 
+        //     elem.tagName === "TEXTAREA"  ) {
+        //         objElem[elem.name] = elem.value;
 
+        //         //очищаем если не radio
+        //         if(elem.type !== 'radio'){
+        //             elem.value = '';
+        //         }
+
+        //     }
+        // } );
+
+        //Способ 3 получам из формы все элементы + значения
+        // for(const elem of formCustomer.elements){
+        //     if(elem.tagName === "INPUT" && elem.type !== "radio" || 
+        //     (elem.type === "radio" && elem.checked) || 
+        //     elem.tagName === "TEXTAREA"  ) {
+        //         objElem[elem.name] = elem.value;
+
+        //         //очищаем если не radio
+        //         if(elem.type !== 'radio'){
+        //             elem.value = '';
+        //         }
+
+        //     }
+        // }
+
+        formCustomer.reset();
+
+        //добавляем элементы формы заказа в массив
         orders.push(objElem);
-        console.log(orders);
-    })
+        console.dir(orders);
+    });
+
+   
     
 })
+
